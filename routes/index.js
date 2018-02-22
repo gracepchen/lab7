ca3358bbd7deef0680b9d42e68b70929bbf188f2
Grace Chen <gpc001@ucsd.edu>
2018-02-22 03:34:17 -0800

    ga

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
 var projects = require('../projects.json');
-
 /*
  * GET home page.
  */
 
-exports.view = function(request, response){
-  	response.render('index', projects);
-  	projects["viewAlt"] = false;
-};
+ exports.view = function(request, response){
+ 	 projects["viewAlt"] = false;
+ 	response.render('index', projects);
+ };
 
-exports.viewAlt = function(request, response){
-  	response.render('index', projects);
-  	projects["viewAlt"] = true;
-};
+ exports.viewAlt = function(request, response){
+ 	projects["viewAlt"] = true;
+ 	response.render('index', projects);
+ };
